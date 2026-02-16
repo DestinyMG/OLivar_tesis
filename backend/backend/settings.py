851292c7d74794b4629fd5ce5a6d31cee05f7409
DestinyMG <DGB0609@gmail.com>
2026-02-15 22:54:43 -0400

    N

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,5 +163,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dgb0609@gmail.com' # Tu correo real
-EMAIL_HOST_PASSWORD = 'mnvs nbny fhxn ikbi'
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD = 
+
