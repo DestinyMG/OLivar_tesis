e5349b8d12f0524b28b06042f8b5f3c79ec896e3
DestinyMG <DGB0609@gmail.com>
2026-02-16 13:03:26 -0400

    pwa

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // Importa la carpeta router
 import './style.css'
+import { registerSW } from 'virtual:pwa-register'
+
 
 const app = createApp(App)
 
 app.use(router) // Le decimos a Vue que use el router
 app.mount('#app')
+
+
+
+registerSW({
+    onOfflineReady() {
+        console.log('PWA lista para usar offline')
+    },
+    onNeedRefresh() {
+        console.log('Nueva versión disponible')
+    }
+})
